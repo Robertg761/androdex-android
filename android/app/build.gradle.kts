89df8211405257b7256bc064bdda3c311b5b6eb5
Robertg761 <robertgordon761@gmail.com>
2026-03-30 01:26:09 -0230

    Bump Android version to 0.2.0 (versionCode 11)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -79,8 +79,8 @@ android {
         applicationId = "io.androdex.android"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.1.0"
+        versionCode = 11
+        versionName = "0.2.0"
         buildConfigField(
             "String",
             "ANDRODEX_DEFAULT_RELAY_URL",
